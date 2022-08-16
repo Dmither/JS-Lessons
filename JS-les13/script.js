84c48b1a0385826f40865adde3eab464bc83b770
Dmither <mr.d.graumann@gmail.com>
2022-08-16 16:36:55 +0300

    Stage13

--- JS-les13/script.js
+++ JS-les13/script.js
@@ -1,15 +1,5 @@
-const html = document.documentElement,
-    head = document.head,
-    body = document.body;
-console.log(html);
-console.log(head);
-console.log(body);
-console.log();
-
-const firstChildBody = body.firstChild,
-    lastChildBody = body.lastChild;
-console.log(firstChildBody);
-console.log(lastChildBody);
-console.log();
-
-console.log(body.children);
+const elemsOne = document.querySelectorAll('.lesson__item');
+console.log(elemsOne);
+for (const item of elemsOne){
+    console.log(item);
+}
