3b9c834632dd66292a7419b05cf50fdf9ac99d35
Dmither <mr.d.graumann@gmail.com>
2022-08-22 15:19:35 +0300

    Stage 16

--- JS-les15/script.js
+++ JS-les15/script.js
@@ -1,3 +1,29 @@
-window.addEventListener('scroll', function(event){
-    console.log(`${scrollY}px`);
+const menuBody = document.querySelector('.menu__body');
+const search = document.querySelector('.txt-item');
+const searchLimit = search.getAttribute('maxlength');
+const searchCounter = document.querySelector('.menu__body span');
+searchCounter.innerHTML = searchLimit;
+
+search.addEventListener("keyup", setCounter);
+search.addEventListener("keydown", function (event){
+    if (event.repeat) setCounter();
 })
+
+document.addEventListener("click", menu);
+document.addEventListener("keydown", function (event){
+    if (event.key === 'Escape') menuBody.classList.remove("menu-active");
+})
+
+function menu(event){
+    if (event.target.closest('.menu__btn')){
+        menuBody.classList.toggle("menu-active");
+    }
+    if (!event.target.closest('.menu')){
+        menuBody.classList.remove("menu-active");
+    }
+}
+
+function setCounter() {
+    const counterResult = searchLimit - search.value.length;
+    searchCounter.innerHTML = counterResult;
+}
